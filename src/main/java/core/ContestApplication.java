85d930a9c9ed936de9c0f89bb0d33ec7cde9420f
22DT <qab3312@gmail.com>
2024-10-26 22:45:24 +0900

    메인합수변경

--- src/main/java/core/ContestApplication.java
+++ src/main/java/core/ContestApplication.java
@@ -1,4 +1,4 @@
-package core.contest.community;
+package core;
 
 import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.Bean;
 
 //@EnableScheduling
 @SpringBootApplication
-public class CommunityApplication {
+public class ContestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommunityApplication.class, args);
+		SpringApplication.run(ContestApplication.class, args);
 	}
 
 	@Bean
